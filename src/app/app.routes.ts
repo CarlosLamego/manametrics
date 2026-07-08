48715833cf4b69582beb450ba09d4778aaf2c3b0
CarlosLamego <clamego1@yahoo.com.br>
2026-07-08 15:58:02 -0300

    feat: create application base structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { Decks } from './features/decks/pages/decks/decks';
+import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        component: Dashboard
+      },
+      {
+        path: 'decks',
+        component: Decks
+      }
+    ]
+  }
+];
