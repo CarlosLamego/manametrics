5cd654619eefceaaf7303416112900793ad123ca
CarlosLamego <clamego1@yahoo.com.br>
2026-07-09 17:56:15 -0300

    feat(layout): cria estrutura base da aplicação

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { Sidebar } from '../components/sidebar/sidebar';
+import { Header } from '../components/header/header';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Header, Sidebar],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
