a063f795dc786748de98a6fcb3c5c0fcc4224059
CarlosLamego <clamego1@yahoo.com.br>
2026-07-16 11:30:28 -0300

    feat(ui): aprimora layout e organiza estilos

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../components/sidebar/sidebar';
 import { Header } from '../components/header/header';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, RouterLink, Header, Sidebar],
+  imports: [RouterOutlet, Header, Sidebar],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
