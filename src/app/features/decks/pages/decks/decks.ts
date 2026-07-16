fe5f1da26a73444a8b8399fef038c4c7937c2c6a
CarlosLamego <clamego1@yahoo.com.br>
2026-07-16 18:10:12 -0300

    feat(decks): implementa estrutura inicial da listagem de decks

--- src/app/features/decks/pages/decks/decks.ts
+++ src/app/features/decks/pages/decks/decks.ts
@@ -1,9 +1,46 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table'; 
 
 @Component({
   selector: 'app-decks',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule, 
+    MatTableModule
+  ],
   templateUrl: './decks.html',
   styleUrl: './decks.scss',
 })
-export class Decks {}
+
+export class Decks {
+
+  displayedColumns: string[] = [
+    'name',
+  ];
+
+dataSource = [
+  {
+    name: 'Izzet Phoenix',
+    format: 'Pioneer',
+    colors: '🔵🔴'
+  },
+  {
+    name: 'Mono Green Devotion',
+    format: 'Pioneer',
+    colors: '🟢'
+  },
+  {
+    name: 'Azorius Control',
+    format: 'Pioneer',
+    colors: '⚪🔵'
+  }
+];
+}
