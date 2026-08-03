66f851a410539de1be56bab519c3a285315a249e
CarlosLamego <clamego1@yahoo.com.br>
2026-08-03 10:50:54 -0300

    feat(decks): persist decks using localStorage

--- src/app/services/deck.service.ts
+++ src/app/services/deck.service.ts
@@ -6,18 +6,65 @@ import { Deck } from '../models/deck.model';
   providedIn: 'root'
 })
 export class DeckService {
-  private readonly importTxtService = inject(ImportTxtService);
+  private readonly _importTxtService = inject(ImportTxtService);
+  private readonly _storageKey = 'manametrics.decks';
+  private _decks: Deck[] = [];
+
+  constructor() {
+    this._load();
+  }
+
+
+  getAll(): Deck[] {
+    return this._decks;
+  }
+
+  add(deck: Pick<Deck, 'name' | 'format'>): void {
+    this._decks.push({
+      id: this._decks.length + 1,
+      name: deck.name,
+      format: deck.format,
+      colors: [],
+      mainboard: [],
+      sideboard: []
+    });
+  }
+
+  getById(id: number): Deck | undefined {
+    return this._decks.find(deck => deck.id === id);
+  }
+
   importTxt(id: number, txt: string): void {
     const deck = this.getById(id);
     if (!deck) {
       return;
     }
-    const importedDeck = this.importTxtService.import(txt);
+    const importedDeck = this._importTxtService.import(txt);
     deck.mainboard = importedDeck.mainboard;
     deck.sideboard = importedDeck.sideboard;
   }
 
-  private decks: Deck[] = [
+
+  private _load(): void {
+    const data = localStorage.getItem(this._storageKey);
+    if (!data) {
+      this._decks = this._createDefaultDecks();
+      this._save();
+      return;
+    }
+    this._decks = JSON.parse(data);
+  }
+
+  private _save(): void {
+    localStorage.setItem(
+      this._storageKey,
+      JSON.stringify(this._decks)
+    );
+  }
+
+
+  private _createDefaultDecks(): Deck[] {
+  return [
     {
       id: 1,
       name: 'Izzet Phoenix',
@@ -43,24 +90,5 @@ export class DeckService {
       sideboard: []
     }
   ];
-
-  getAll(): Deck[] {
-    return this.decks;
-  }
-
-  add(deck: Pick<Deck, 'name' | 'format'>): void {
-    this.decks.push({
-      id: this.decks.length + 1,
-      name: deck.name,
-      format: deck.format,
-      colors: [],
-      mainboard: [],
-      sideboard: []
-    });
-  }
-
-  getById(id: number): Deck | undefined {
-    return this.decks.find(deck => deck.id === id);
-  }
-
+}
 }
